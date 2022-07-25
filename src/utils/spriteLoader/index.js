6202b98c7bdaa220a67a1c4d2eb9b8b9541eed23
MSpilari <mb.spilari@hotmail.com>
2022-07-25 17:56:49 -0300

    Criando o spriteloader

---
+++ src/utils/spriteLoader/index.js
@@ -0,0 +1,7 @@
+import bgGreenHills from '../../assets/background/sky_background_green_hills.png'
+
+const spriteLoader = () => {
+	loadSprite('bgGreenHills', bgGreenHills)
+}
+
+export { spriteLoader }
