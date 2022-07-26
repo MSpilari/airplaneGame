bd7605f3009e0de9c9d29a18011ac39d5ade9f36
MSpilari <mb.spilari@hotmail.com>
2022-07-26 17:49:34 -0300

    Mostrando aviões em posições aleatórias na tela

--- src/scenes/menu/index.js
+++ src/scenes/menu/index.js
@@ -1,20 +1,56 @@
 const menu = () => {
-	add([sprite('bgGreenHills', { width: width(), height: height() }, z(-1))])
+	const planes = [
+		'plane1Blue',
+		'plane1Pink',
+		'plane1Red',
+		'plane1Yellow',
+		'plane2Yellow',
+		'plane2Blue',
+		'plane2Red',
+		'plane2Green'
+	]
 
-	add([
-		text("SHOOT N' FLY"),
+	let bgMenu = add([
+		sprite('bgGreenHills', { width: width(), height: height() }, z(-1))
+	])
+
+	let GameName = add([
+		text("SHOOT\nN'\nFLY"),
 		color(RED),
-		pos(width() / 2, height() / 2),
+		pos(center()),
 		origin('center')
 	])
 
-	add([
-		pos(width() / 2 - 200, height() - 100),
+	let buttonRect = add([
+		pos(width() / 2, (3 * height()) / 4),
 		rect(200, 40),
-		outline(4, RED),
-		color(YELLOW),
-		text('Fly')
+		color(RED),
+		origin('center'),
+		'bgButton',
+		area()
 	])
+
+	let buttonText = add([
+		text('Start', { size: 20 }),
+		pos(width() / 2, (3 * height()) / 4),
+		origin('center')
+	])
+
+	loop(3, () => {
+		let planeSpeed = randi(0, 2) % 2 === 0 ? -300 : 300
+		let posX = planeSpeed < 0 ? width() : -width()
+		add([
+			sprite(planes[randi(planes.length)], {
+				width: 150,
+				height: 80,
+				flipX: planeSpeed < 0
+			}),
+			move((20, 0), planeSpeed),
+			pos(posX, rand(height()))
+		])
+	})
+
+	onClick('bgButton', () => go('game'))
 }
 
 export { menu }
