2e8c3245229cda9d5e3f1b9de08a7f2ef7f90879
MSpilari <mb.spilari@hotmail.com>
2022-07-27 17:45:56 -0300

    Jogador agora pode lançar mísseis

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,16 +1,32 @@
+import { bullet } from '../bullet'
+
 const player = (spriteID, widthSprite, heightSprite) => {
 	let playerChar = add([
 		sprite(spriteID, { width: widthSprite, height: heightSprite }),
-		pos(0, height() / 2),
-		area()
+		pos(30, height() / 2),
+		area(),
+		'player',
+		health(100)
 	])
 
-	keyDown('up', () => {
+	onKeyDown('up', () => {
 		playerChar.pos.y >= 0 && playerChar.move(0, -500)
 	})
 
-	keyDown('down', () => {
+	onKeyDown('down', () => {
 		playerChar.pos.y <= height() && playerChar.move(0, 500)
+	})
+
+	onKeyDown('left', () => {
+		playerChar.pos.x >= 0 && playerChar.move(-500, 0)
+	})
+
+	onKeyDown('right', () => {
+		playerChar.pos.x < width() && playerChar.move(500, 0)
+	})
+
+	onKeyDown('space', () => {
+		bullet('fireball', 80, 40, 'Player', playerChar.pos.x, playerChar.pos.y)
 	})
 }
 
