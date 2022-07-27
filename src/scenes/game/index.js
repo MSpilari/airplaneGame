100268f8666ba66159456993bd92e555d2114e37
MSpilari <mb.spilari@hotmail.com>
2022-07-27 13:06:44 -0300

    Inserindo o player no game scene

--- src/scenes/game/index.js
+++ src/scenes/game/index.js
@@ -1,7 +1,10 @@
 import { Background } from '../../components/background'
+import { player } from '../../components/player'
 
 const game = () => {
 	Background('bgGreenHills', true)
+
+	player('plane1Blue', 150, 80)
 }
 
 export { game }
