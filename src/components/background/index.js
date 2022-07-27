bde0b6fdbcb60fa87283ebb5337db7bb2c67e20c
MSpilari <mb.spilari@hotmail.com>
2022-07-27 12:20:52 -0300

    Inserindo o movimento condicional do bg

--- src/components/background/index.js
+++ src/components/background/index.js
@@ -1,23 +1,26 @@
-const Background = bgID => {
-	let bg1 = add([
-		sprite(bgID, { width: width(), height: height(), flipX: true }),
-		pos(width(), 0),
-		move((20, 0), -100)
-	])
-
+const Background = (bgID, isMoving) => {
 	let bg2 = add([
 		sprite(bgID, {
 			width: width(),
 			height: height()
 		}),
 		pos(0, 0),
-		move((20, 0), -100)
+		isMoving && move((20, 0), -100)
 	])
 
-	onUpdate(() => {
-		if (bg2.pos.x <= -width()) bg2.pos.x = width() - 3
-		if (bg1.pos.x <= -width()) bg1.pos.x = width() - 3
-	})
+	let bg1 =
+		isMoving &&
+		add([
+			sprite(bgID, { width: width(), height: height(), flipX: true }),
+			pos(width(), 0),
+			move((20, 0), -100)
+		])
+
+	isMoving &&
+		onUpdate(() => {
+			if (bg2.pos.x <= -width()) bg2.pos.x = width() - 3
+			if (bg1.pos.x <= -width()) bg1.pos.x = width() - 3
+		})
 }
 
 export { Background }
