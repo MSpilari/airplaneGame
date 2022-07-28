fa74af343d94fe631ae570be6cce40f7f8003275
MSpilari <mb.spilari@hotmail.com>
2022-07-28 17:45:38 -0300

    Adicionando a cena de gameOver

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import kaboom from 'kaboom'
 import { game } from './src/scenes/game'
+import { gameOver } from './src/scenes/gameOver'
 import { menu } from './src/scenes/menu'
 import { spriteLoader } from './src/utils/spriteLoader'
 import './style.css'
@@ -14,5 +15,7 @@ spriteLoader()
 scene('menu', () => menu())
 
 scene('game', () => game())
+
+scene('gameOver', () => gameOver())
 
 go('menu')
