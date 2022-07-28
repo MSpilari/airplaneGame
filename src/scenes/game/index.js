d3394f088dc6eeccc866304b3c3f94bcc06a08c1
MSpilari <mb.spilari@hotmail.com>
2022-07-28 17:48:27 -0300

    Adicionando o indicador de vida na cena do game

--- src/scenes/game/index.js
+++ src/scenes/game/index.js
@@ -1,10 +1,12 @@
 import { Background } from '../../components/background'
+import { healthIndicator } from '../../components/healthIndicator'
 import { player } from '../../components/player'
 
 const game = () => {
 	Background('bgGreenHills', true)
 
 	player('plane1Blue', 150, 80)
+	healthIndicator()
 }
 
 export { game }
