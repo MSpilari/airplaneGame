158277975c943791f5b0c7f3065150431e8e00e4
MSpilari <mb.spilari@hotmail.com>
2022-07-29 17:58:38 -0300

    Adicionando a colisão do enemy com bullet

--- src/components/bullet/index.js
+++ src/components/bullet/index.js
@@ -7,13 +7,18 @@ const bullet = (spriteID, widthSprite, heightSprite, shooter, posX, posY) => {
 		}),
 		area(),
 		pos(posX + 150, posY),
-		move((100, 0), 400),
+		shooter === 'Enemy' ? move((100, 0), -400) : move((100, 0), 400),
 		'bullet'
 	])
 
 	onCollide('player', 'bullet', (player, bullet) => {
 		bullet.destroy()
 		player.hurt(25)
+	})
+
+	onCollide('enemy', 'bullet', (enemy, bullet) => {
+		bullet.destroy()
+		enemy.hurt(25)
 	})
 }
 
