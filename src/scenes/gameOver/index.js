17a0b0586c37aace2aefc6fdb3444619f0a6d224
MSpilari <mb.spilari@hotmail.com>
2022-08-01 18:03:31 -0300

    Adicionando temporariamente a animação de explosão

--- src/scenes/gameOver/index.js
+++ src/scenes/gameOver/index.js
@@ -4,11 +4,20 @@ import { button } from '../../components/button'
 const gameOver = () => {
 	Background('bgGreenHills', false)
 
-	add([text('Game Over !!'), pos(center())])
+	add([text('Game Over !!'), pos(center()), origin('center')])
 
 	button('Restart')
 
 	onClick('bgButton', () => go('game'))
+
+	// add([
+	// 	sprite('planeExplosion', {
+	// 		width: 150,
+	// 		height: 80,
+	// 		animSpeed: 1,
+	// 		anim: 'explode'
+	// 	})
+	// ])
 }
 
 export { gameOver }
