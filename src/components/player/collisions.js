b0ce7a5597fff8d7f9677630f9f04d40072ce4a9
MSpilari <mb.spilari@hotmail.com>
2022-08-02 17:07:21 -0300

    Adicionando a colisão player-littleEnemy

--- src/components/player/collisions.js
+++ src/components/player/collisions.js
@@ -1,5 +1,6 @@
 const playerCollisions = () => {
 	onCollide('player', 'bullet', player => player.hurt(25))
+	onCollide('player', 'littleEnemy', player => player.hurt(10))
 }
 
 export { playerCollisions }
