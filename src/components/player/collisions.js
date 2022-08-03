28e04540879d628d48a096470b90b1df8a715951
MSpilari <mb.spilari@hotmail.com>
2022-08-03 17:06:40 -0300

    Adicionado colisão player-coin

--- src/components/player/collisions.js
+++ src/components/player/collisions.js
@@ -1,6 +1,13 @@
 const playerCollisions = () => {
 	onCollide('player', 'bullet', player => player.hurt(25))
+
 	onCollide('player', 'littleEnemy', player => player.hurt(10))
+
+	onCollide('player', 'bronzeCoinUD', player => player.heal(5))
+
+	onCollide('player', 'silverCoinUD', player => player.heal(15))
+
+	onCollide('player', 'goldCoinUD', player => player.heal(20))
 }
 
 export { playerCollisions }
