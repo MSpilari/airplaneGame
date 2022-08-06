ae986970207c930896762a06ea964b3fbf3da551
MSpilari <mb.spilari@hotmail.com>
2022-08-06 14:00:13 -0300

    Adicionando o efeito sonoro ao atirar

--- src/components/player/shoots.js
+++ src/components/player/shoots.js
@@ -2,8 +2,10 @@ import { bullet } from '../bullet'
 
 const playerShoots = playerChar => {
 	onKeyPress('space', () => {
-		playerChar.exists() &&
+		if (playerChar.exists()) {
 			bullet('fireball', 80, 40, 'Player', playerChar.pos.x, playerChar.pos.y)
+			play('playerShootSound', { volume: 0.2 })
+		}
 	})
 }
 
