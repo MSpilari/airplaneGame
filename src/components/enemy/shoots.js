687c74b7259edc3fc00572a263ce609353b78dcf
MSpilari <mb.spilari@hotmail.com>
2022-08-09 12:11:32 -0300

    Tocando o som do tiro quando o inimigo atirar

--- src/components/enemy/shoots.js
+++ src/components/enemy/shoots.js
@@ -2,8 +2,10 @@ import { bullet } from '../bullet'
 
 const enemyShoots = enemyChar => {
 	loop(3, () => {
-		enemyChar.exists() &&
+		if (enemyChar.exists()) {
 			bullet('fireballBlue', 80, 40, 'Enemy', enemyChar.pos.x, enemyChar.pos.y)
+			play('enemyShootSound', { volume: 0.4 })
+		}
 	})
 }
 
