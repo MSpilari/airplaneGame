93bf6633cbb34a5dcae4b7243746f7a57e9b05d6
MSpilari <mb.spilari@hotmail.com>
2022-08-11 17:10:28 -0300

    Retirando o planeNames de dentro do menu

--- src/scenes/menu/index.js
+++ src/scenes/menu/index.js
@@ -1,22 +1,8 @@
 import { Background } from '../../components/background'
 import { button } from '../../components/button'
+import { planes } from '../../utils/planesNames'
 
 const menu = () => {
-	const planes = [
-		'plane1Blue',
-		'plane1Pink',
-		'plane1Red',
-		'plane1Yellow',
-		'plane2Yellow',
-		'plane2Blue',
-		'plane2Red',
-		'plane2Green',
-		'plane3Yellow',
-		'plane3Blue',
-		'plane3Red',
-		'plane3Green'
-	]
-
 	Background('bgGreenHills', false)
 
 	let GameName = add([
