e01eeb999836b45e3386a86b8e471bfbc6e91768
MSpilari <mb.spilari@hotmail.com>
2022-08-11 17:10:52 -0300

    Criando um arquivo apenas com os planeNames

---
+++ src/utils/planesNames/index.js
@@ -0,0 +1,16 @@
+const planes = [
+	'plane1Blue',
+	'plane1Pink',
+	'plane1Red',
+	'plane1Yellow',
+	'plane2Yellow',
+	'plane2Blue',
+	'plane2Red',
+	'plane2Green',
+	'plane3Yellow',
+	'plane3Blue',
+	'plane3Red',
+	'plane3Green'
+]
+
+export { planes }
