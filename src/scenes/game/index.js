d689e83ac918b8c624a0e6afa77c7c22f54ed1f9
MSpilari <mb.spilari@hotmail.com>
2022-08-11 17:09:40 -0300

    Em caso de morte do enemy, outro renasce

--- src/scenes/game/index.js
+++ src/scenes/game/index.js
@@ -4,14 +4,25 @@ import { enemy } from '../../components/enemy'
 import { healthIndicator } from '../../components/healthIndicator'
 import { littleEnemies } from '../../components/littleEnemies'
 import { player } from '../../components/player'
+import { planes } from '../../utils/planesNames'
 
 const game = () => {
 	Background('bgGreenHills', true)
 
 	player('plane1Blue', 150, 80)
 	healthIndicator()
+
 	enemy('plane2Yellow', 150, 80)
+
+	onUpdate('enemy', enemyChar => {
+		if (!enemyChar.exists()) {
+			let planeIndex = randi(0, planes.length)
+			wait(5, () => enemy(planes[planeIndex], 150, 80))
+		}
+	})
+
 	loop(3, () => littleEnemies(100, 50))
+
 	loop(30, () => coins(100, 70))
 }
 
