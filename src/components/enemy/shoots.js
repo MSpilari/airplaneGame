30cbddf04ed0651f40f192c3e45c4ec5d2c98cee
MSpilari <mb.spilari@hotmail.com>
2022-08-11 17:08:46 -0300

    Alterando o tempo de tiro do inimigo

--- src/components/enemy/shoots.js
+++ src/components/enemy/shoots.js
@@ -1,7 +1,7 @@
 import { bullet } from '../bullet'
 
 const enemyShoots = enemyChar => {
-	loop(3, () => {
+	loop(1, () => {
 		if (enemyChar.exists()) {
 			bullet('fireballBlue', 80, 40, 'Enemy', enemyChar.pos.x, enemyChar.pos.y)
 			play('enemyShootSound', { volume: 0.4 })
